050dbf3f556ca025705f19d0ba35414b301a281d
Gabriel Casarini <139912687+gabrielhnc@users.noreply.github.com>
2024-11-14 19:02:00 -0300

    Update funcionario.py
    ajuste nos atributos e métodos Get e Set

--- aplicacao_python/funcionario.py
+++ aplicacao_python/funcionario.py
@@ -3,28 +3,29 @@ from pessoa import Pessoa
 #Classe herdada da classe Pessoa (Herança)
 class Funcionario(Pessoa):
     #Metodo Construtor
-    def __init__(self, nome, cpf, salario, idtipo = None, idfuncionario = None):
+    def __init__(self, nome, cpf, salario, idcargo = None, idfuncionario = None):
         super().__init__(nome, cpf)
         self.__salario = salario
-        self.__idtipo = idtipo
+        self.__idcargo = idcargo
         self.__idfuncionario = idfuncionario #(Encapsulamento)
 
     #Get
     def get_salario(self):
         return self.__salario
 
-    def get_idtipo(self):
-        return self.__idtipo
+    def get_idcargo(self):
+        return self.__idcargo
     
     def get_idfuncionario(self):
         return self.__idfuncionario
 
     #Set
-    def set_idtipo(self, idtipo):
-        self.__idtipo = idtipo
+    def set_idcargo(self, idcargo):
+        self.__idcargo = idcargo
 
     def set_salario(self, salario):
         self.__salario = salario
 
     def set_idfuncionario(self, idfuncionario):
         self.__idfuncionario = idfuncionario
+
